85f24a9231b3c074f2f8d9a4d52479646625b242
Samannaway <samannawayghosh@gmail.com>
2021-12-23 21:46:47 +0530

    my circumference website ;)
    only for fun (NOT FOR ANY OTHER PURPOSE)

---
+++ script.js
@@ -0,0 +1,23 @@
+let inputBtn = document.getElementById("input")
+let radius = document.getElementById("number-input")
+let PI = 22/7
+let PI2 = Math.PI
+let circumference;
+let output = document.getElementById("output");
+
+inputBtn.addEventListener("click",()=>{
+
+    radius = parseInt(radius.value)
+    circumference = 2*(radius*PI)
+    let circumference2 = 2*(radius*PI2)
+
+    if ( Number.isInteger(radius/4) == true) {
+        output.innerHTML = "this is the circumference - " +  circumference2
+    }else{
+        output.innerHTML = "this is the circumference - " +  circumference
+    }
+
+    console.log(circumference);
+    console.log(radius);
+    radius=document.getElementById("number-input")
+})
